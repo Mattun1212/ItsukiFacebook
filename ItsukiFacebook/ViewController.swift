d45a376021620115ba5eb2e2d1b488ca82d44fce
Mattun1212 <s1911437@s.tsukuba.ac.jp>
2022-01-19 19:02:37 +0900

    Initial Commit

---
+++ ItsukiFacebook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItsukiFacebook
+//
+//  Created by Koutaro Matsushita on 2022/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
